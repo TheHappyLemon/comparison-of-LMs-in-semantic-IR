2a01f96c9cead813c41b798228510d8bb3934a5a
Artem <artem.kucheravy@gmail.com>
2024-09-11 20:40:48 +0300

    Merge LASER embeddings from bachelor work to common file

--- Code/join_embeddings.py
+++ Code/join_embeddings.py
@@ -8,7 +8,7 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
     handlers=[
-        logging.FileHandler(PATH_LOGS + "merge.log")
+        logging.FileHandler(PATH_LOGS + "merge_LASER.log")
     ]
 )
 
@@ -16,10 +16,13 @@ def copy_dataset(source_file_path, source_dataset_path, target_file_path, target
     with h5py.File(source_file_path, 'r') as source_file:
         data = source_file[source_dataset_path]
         with h5py.File(target_file_path, 'a') as target_file:
+            #if target_dataset_path in target_file:
+                #del target_file[target_dataset_path]
+                #print('deleted', target_dataset_path)
             target_file.create_dataset(target_dataset_path, data=data)
             logger.info(f"Data copied from file {source_file_path} dataset {source_dataset_path} to file {target_dataset_path} dataset {target_file_path}")
         
-source_files = {
+'''source_files = {
     'embeddings_sonar_en_open.hdf5'  : '/SONAR/open/en_cirrussearch',
     'embeddings_sonar_en_source.hdf5': '/SONAR/source/en_cirrussearch',
     'embeddings_sonar_en_title.hdf5' : '/SONAR/title/en_cirrussearch',
@@ -27,9 +30,20 @@ source_files = {
     'embeddings_sonar_lv_source.hdf5': '/SONAR/source/lv_cirrussearch',
     'embeddings_sonar_lv_title.hdf5' : '/SONAR/title/lv_cirrussearch'
 }
+'''
+
+source_files = {
+    '1' : '/LASER/open/en_cirrussearch',
+    '2' : '/LASER/open/lv_cirrussearch',
+    '3' : '/LASER/source/en_cirrussearch',
+    '4' : '/LASER/source/lv_cirrussearch',
+    '5' : '/LASER/title/en_cirrussearch',
+    '6' : '/LASER/title/lv_cirrussearch'
+}
+
 target_file = PATH_DATASET + 'embeddings.hdf5'
 
 for source_file in source_files:
     logger = logging.getLogger(__name__)
-    copy_dataset(PATH_DATASET + source_file, source_files[source_file], target_file, source_files[source_file])
+    copy_dataset(PATH_DATASET + 'embeddings_old.hdf5', source_files[source_file], target_file, source_files[source_file])
     
